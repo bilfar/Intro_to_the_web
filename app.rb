c16c32d10aa5f27900bc933142115266584d6b84
nabil far <nabil.far.contact@gmail.com>
2018-06-26 12:34:13 +0100

    separated html from the routing

--- app.rb
+++ app.rb
@@ -9,9 +9,6 @@ require 'sinatra'
 #   "This is secret message"
 # end
 
-require 'sinatra'
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-     <img src='http://bit.ly/1eze8aE'>
-   </div>"
+  erb(:index)
 end
